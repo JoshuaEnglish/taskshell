f885528e4713552123490302f9109f6b899e8938
Josh English <joshua.r.english@gmail.com>
2022-03-09 16:06:31 -0800

    Found a bug in quotidia

--- taskshell/plugins/quotidia.py
+++ taskshell/plugins/quotidia.py
@@ -23,27 +23,33 @@ import minioncmd
 
 from taskshell import lister
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
 quotidiaparser = parser = argparse.ArgumentParser(
-    'quotidia', description="Manage regularly scheduled tasks")
+    "quotidia", description="Manage regularly scheduled tasks"
+)
 quotidia_command = parser.add_subparsers(
-    title="Quotidia Commands", dest='subcommand', metavar='')
+    title="Quotidia Commands", dest="subcommand", metavar=""
+)
 
 
-list_cmd = quotidia_command.add_parser(
-    'list', help='lists current quotidia')
-list_cmd.add_argument(
-    'columns', nargs=argparse.REMAINDER, help="optional extra colums")
+list_cmd = quotidia_command.add_parser("list", help="lists current quotidia")
+list_cmd.add_argument("columns", nargs=argparse.REMAINDER, help="optional extra colums")
 
 # these arguments should be standard with any plugins
 directory = quotidiaparser.add_argument(
-    '--directory', action="store_true",
-    default=False, help='show directory of the quotidia and quit')
+    "--directory",
+    action="store_true",
+    default=False,
+    help="show directory of the quotidia and quit",
+)
 
 version = quotidiaparser.add_argument(
-    '--version', action="store_true",
-    default=False, help='show version of the quotidia plugin and quit')
+    "--version",
+    action="store_true",
+    default=False,
+    help="show version of the quotidia plugin and quit",
+)
 
 
 class QuotidiaCmd(minioncmd.MinionCmd):
@@ -53,8 +59,9 @@ class QuotidiaCmd(minioncmd.MinionCmd):
     Manage scheduled tasks"""
 
     def __init__(self, completekey="tab", stdin=None, stdout=None, qlib=None):
-        super().__init__('quotidia', completekey=completekey, stdin=stdin,
-                         stdout=stdout)
+        super().__init__(
+            "quotidia", completekey=completekey, stdin=stdin, stdout=stdout
+        )
         self.lib = qlib
 
     def do_list(self, text):
@@ -64,12 +71,11 @@ class QuotidiaCmd(minioncmd.MinionCmd):
         such as `last_run` or `task_text`
         """
         fields = text.split()
-        if 'qid' not in fields:
-            fields.insert(0, 'qid')
+        if "qid" not in fields:
+            fields.insert(0, "qid")
         fields.extend([])
         getter = operator.attrgetter(*fields)
-        lister.print_list([getter(q) for q in self.lib.qids.values()],
-                          fields)
+        lister.print_list([getter(q) for q in self.lib.qids.values()], fields)
 
     def do_new(self, text):
         """Usage: new NAME
@@ -83,7 +89,7 @@ class QuotidiaCmd(minioncmd.MinionCmd):
         newname = newname[0]
 
         if newname in self.lib.qids:
-            self.lib.log.error('Quotidia %s already exists', newname)
+            self.lib.log.error("Quotidia %s already exists", newname)
             print(f"Quotidia `{newname}` already exists")
             return None
         text = input("Enter text for new quotidium: ")
@@ -102,14 +108,27 @@ class QuotidiaCmd(minioncmd.MinionCmd):
         self.lib.add_quotidia(newname, text, days)
         print("Created new quotidia in", fname)
 
+    def do_activate(self, text):
+        """Usage: activate QID
+
+        Activate a given quotidia"""
+        self.lib.activate_quotidium(text)
+
+    def do_deactivate(self, text):
+        """deactivate QID
+
+        Deactivates a quotidium"""
+        self.lib.deactivate_quotidium(text)
+
 
 class Quotidium:
     """Quotidium(qid: str, text: str, days: str, active: bool=True,
-                 history=None)
-       Create a Quotidium object"""
+              history=None)
+    Create a Quotidium object"""
 
-    def __init__(self, qid: str, text: str, days: str,
-                 active: bool = True, history=None):
+    def __init__(
+        self, qid: str, text: str, days: str, active: bool = True, history=None
+    ):
 
         self.qid = qid
         self.text = text
@@ -123,13 +142,13 @@ class Quotidium:
     @property
     def as_dict(self):
         return {
-            '__quotidium__': True,
-            'qid': self.qid,
-            'text': self.text,
-            'days': self.days,
-            'active': self.active,
-            'history': [d.isoformat() for d in self.history]
-            }
+            "__quotidium__": True,
+            "qid": self.qid,
+            "text": self.text,
+            "days": self.days,
+            "active": self.active,
+            "history": [d.isoformat() for d in self.history],
+        }
 
     def __str__(self):
         return f"{self.qid}: `{self.text}` on {self.days}"
@@ -153,6 +172,7 @@ class Quotidium:
 
 class QuotidiaEncoder(json.JSONEncoder):
     """Custome JSON Encoder"""
+
     def default(self, quotidium):
         if isinstance(quotidium, Quotidium):
             return quotidium.as_dict
@@ -162,11 +182,15 @@ class QuotidiaEncoder(json.JSONEncoder):
 
 def load_quotidium(dct):
     if "__quotidium__" in dct:
-        if 'active' not in dct:
-            dct['active'] = True
-        return Quotidium(dct['qid'], dct['text'], dct['days'], dct['active'],
-                         [datetime.date.fromisoformat(d)
-                             for d in dct['history']])
+        if "active" not in dct:
+            dct["active"] = True
+        return Quotidium(
+            dct["qid"],
+            dct["text"],
+            dct["days"],
+            dct["active"],
+            [datetime.date.fromisoformat(d) for d in dct["history"]],
+        )
     return dct
 
 
@@ -178,10 +202,10 @@ class QuotidiaLib(object):
     __version__ = __version__
 
     def __init__(self, directory):
-        self.log = logging.getLogger('quotidia')
-        self.log.info('creating quotidialib')
+        self.log = logging.getLogger("quotidia")
+        self.log.info("creating quotidialib")
 
-        self.directory = pathlib.Path(directory) / 'quotidia'
+        self.directory = pathlib.Path(directory) / "quotidia"
         self.directory.mkdir(exist_ok=True)
 
         self.qids = {}
@@ -194,8 +218,9 @@ class QuotidiaLib(object):
 
     def get_quotidia(self):
         for qfile in list(self.directory.glob("*.quotidia")):
-            self.qids[qfile.stem] = json.loads(qfile.read_text(),
-                                               object_hook=load_quotidium)
+            self.qids[qfile.stem] = json.loads(
+                qfile.read_text(), object_hook=load_quotidium
+            )
 
     def add_quotidia(self, qid, text, days):
         if qid in self.qids:
@@ -204,26 +229,25 @@ class QuotidiaLib(object):
             return None
         q = Quotidium(qid, text, days)
         fname = f"{qid}.quotidia"
-        json.dump(q, (self.directory / fname).open(mode='w'),
-                  cls=QuotidiaEncoder)
-        self.log.info('Created %s', fname)
+        json.dump(q, (self.directory / fname).open(mode="w"), cls=QuotidiaEncoder)
+        self.log.info("Created %s", fname)
         self.qids[qid] = q
 
     def save_quotidium(self, q):
         fname = f"{q.qid}.quotidia"
-        with open(self.directory / fname, 'w') as qf:
+        with open(self.directory / fname, "w") as qf:
             json.dump(q, qf, cls=QuotidiaEncoder)
-        self.log.info('Saved %s', fname)
+        self.log.info("Saved %s", fname)
 
     def activate_quotidium(self, qid):
         q = self.qids[qid]
-        self.log.debug('Activating quotidium %s', qid)
+        self.log.debug("Activating quotidium %s", qid)
         q.active = True
         self.save_quotidium(q)
 
     def deactivate_quotidium(self, qid):
         q = self.qids[qid]
-        self.log.debug('Deactivating quotidium %s', qid)
+        self.log.debug("Deactivating quotidium %s", qid)
         q.active = False
         self.save_quotidium(q)
 
@@ -240,19 +264,19 @@ class QuotidiaLib(object):
             if not q.active:
                 continue
             days_since_run = (today - q.last_run).days
-            self.log.debug('checking qid %s', qid)
-            self.log.debug("%s last run on %s (%s days ago)",
-                           qid, q.last_run, days_since_run)
-            if q.recurrencetype == 'DOW':
-                self.log.debug('Trying by Day of Week')
-                should_have_run = min(
-                    (daysago(today.weekday(), d) for d in q.days))
-                if (should_have_run < days_since_run):
+            self.log.debug("checking qid %s", qid)
+            self.log.debug(
+                "%s last run on %s (%s days ago)", qid, q.last_run, days_since_run
+            )
+            if q.recurrencetype == "DOW":
+                self.log.debug("Trying by Day of Week")
+                should_have_run = min((daysago(today.weekday(), d) for d in q.days))
+                if should_have_run < days_since_run:
                     tasks_to_add.add((qid, q))
 
-            if q.recurrencetype == 'DOM':
-                self.log.debug('trying by day of month')
-                for day in sorted(q.days.split(';')):
+            if q.recurrencetype == "DOM":
+                self.log.debug("trying by day of month")
+                for day in sorted(q.days.split(";")):
                     dom = int(day)
                     if today.day < dom:
                         continue
@@ -273,8 +297,8 @@ class QuotidiaLib(object):
 
     def run_quotidium(self, qid):
         if qid not in self.qids:
-            self.log.critical('Cannot run qid %s: does not exist', qid)
-            raise ValueError('Cannot run qid %s: does not exist' % qid)
+            self.log.critical("Cannot run qid %s: does not exist", qid)
+            raise ValueError("Cannot run qid %s: does not exist" % qid)
         self.qids[qid].history.insert(0, datetime.date.today())
         self.save_quotidium(self.qids[qid])
 
